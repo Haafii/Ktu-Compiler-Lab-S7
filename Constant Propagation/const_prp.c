c1dbad188cfbc07ae30f39b4a877720a937ba3e1
Haafii <hafismuhammedtk@gmail.com>
2023-12-12 23:50:49 +0530

    updated constant propagation

--- Constant Propagation/const_prp.c
+++ Constant Propagation/const_prp.c
@@ -1,52 +1,45 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-
-int main() {
-    FILE *fp1, *fp2;
-    int i = 0, count = 0, num;
-    char ch, nums[10] = {0};
-
-    fp1 = fopen("inputfile.c", "r");
-    fp2 = fopen("outputfile.c", "w");
-
-    while ((ch = fgetc(fp1)) != EOF) {
-        if (ch == 'x') {
-        	if(count==0){
-        		fprintf(fp2,"%s","x=");
-        	}
-            ch = fgetc(fp1);
-            if (ch == '=') {
-                while (ch != ';') {
+int main(){
+    FILE *fp1,*fp2;
+    int i=0,flag=0,num;
+    char ch,nums[10]={0};
+    fp1=fopen("inputfile.c","r");
+    fp2=fopen("outputfile.c","w");
+    while(!feof(fp1)){
+        ch = fgetc(fp1);
+        if(ch == 'x'){
+            if(flag == 0){
+                fprintf(fp2, "%c", ch);
+                ch = fgetc(fp1);
+                if(ch == '='){
+                    flag = 1;
+                    fprintf(fp2, "%c", ch);
                     ch = fgetc(fp1);
-                    nums[i] = ch;
-                    i++;
+                    while(ch != ';'){
+                        fprintf(fp2, "%c", ch);
+                        nums[i] = ch;
+                        i++;
+                        ch = fgetc(fp1);
+                    }
+                    nums[i] = '\0';
                 }
-                nums[i - 1] = '\0';
                 num = atoi(nums);
-                fprintf(fp2, "%d", num);
-                if(ch==';'){
-                	fprintf(fp2,"%c",';');
-                }
-            } else {
-                fseek(fp1, -1, SEEK_CUR);
             }
-
-            if (count > 0) {
+            else if(flag == 1){
                 fprintf(fp2, "%d", num);
             }
-            count++;
-        } else {
-            fprintf(fp2, "%c", ch);
         }
+        if(ch != 'x')
+            fprintf(fp2, "%c", ch);
+        if(ch == '}')
+            break;
     }
-
     fclose(fp1);
     fclose(fp2);
-
     return 0;
 }
-
 
 
 
