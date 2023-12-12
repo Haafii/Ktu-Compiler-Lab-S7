c0d8bcfccf340959316e0fa3c3026f8a05bb03d0
Haafii <hafismuhammedtk@gmail.com>
2023-12-12 22:06:32 +0530

    Design and implement a lexical analyzer using LEX to recognize valid tokens (identifiers, keywords, operators, constants etc. . .) in the input C program. The lexical analyzer should ignore redundant spaces, tabs and newlines. It should also ignore comments.

---
+++ Lex/lexical analyzer using LEX/Hello.c
@@ -0,0 +1,10 @@
+/*this in multiline comment
+   We are going to remove this*/
+
+   // this is single line comment
+   int main()
+   {
+       int a,b=10;
+       char buffer[100];
+   printf("Hello World");
+   } 
